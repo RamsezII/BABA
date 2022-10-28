59c6273fc6cc22ec9018553c1583aab963014246
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2022-10-28 19:32:34 +0200

    Creation des methodes Up,down,left,right et creation de map une liste 2D 3x3 (pour tester)

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
 
-
 print("waw")
+
+
+
+#### Mouvement ####
+def move_up():
+    return 0
+
+def move_down():
+    return 0
+def move_left():
+    return 0
+
+def move_right():
+    return 0
+
+
+#### La MAP ####
+
+map=[[0,0,0],
+     [0,0,0],
+     [0,0,0]]
+
